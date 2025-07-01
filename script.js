309251af0ae0cc55923e7b087f859c30603afc00
Vishal Mavinmar <vishalmavinmar3@gmail.com>
2025-07-01 09:55:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+ var typed = new Typed('#element', {
+            strings: ['Web Developer', 'Passionate Python developer', 'Data Analyst', 'Java Developer','Software Developer'],
+            typeSpeed: 50,
+        });
