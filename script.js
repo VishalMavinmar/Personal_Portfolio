2cc91fc025a19015bb0bfbd89d3c413dbc3658b3
Vishal Mavinmar <vishalmavinmar3@gmail.com>
2025-07-02 08:42:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
             strings: ['Web Developer', 'Passionate Python developer', 'Data Analyst', 'Java Developer','Software Developer'],
             typeSpeed: 50,
         });
+
+// const menuIcon = document.getElementById('menu-icon');
+// const navLinks = document.getElementById('nav-links');
+
+// menuIcon.addEventListener('click', () => {
+//     navLinks.classList.toggle('show');
+// });
+
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    document.getElementById('nav-menu').classList.toggle('show');
+});
